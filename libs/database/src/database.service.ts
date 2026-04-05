ce05083aacdca30f13b6a3b0e1786f063d0d4ddd
swopnil <108117980+swopnil8848@users.noreply.github.com>
2026-04-05 21:17:40 +0545

    feat: Configure DB setup

---
+++ libs/database/src/database.service.ts
@@ -0,0 +1,34 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { drizzle, NodePgDatabase } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
+import * as schema from './schema';
+
+@Injectable()
+export class DatabaseService implements OnModuleInit, OnModuleDestroy {
+  private pool?: Pool;
+  public db?: NodePgDatabase<typeof schema>;
+
+  async onModuleInit() {
+    const connectionString = process.env.DATABASE_URL;
+
+    if (!connectionString) {
+      throw new Error('DATABASE_URL is not set');
+    }
+
+    this.pool = new Pool({ connectionString });
+    this.db = drizzle(this.pool, { schema });
+
+    console.log('database connected');
+  }
+
+  async onModuleDestroy() {
+    if (this.pool) {
+      await this.pool.end();
+    }
+  }
+
+  get schema() {
+    return schema;
+  }
+}
+
