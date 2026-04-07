6cc5af00aa3cbb5c1a4b45bc6ffb94890eb09cca
swopnil <108117980+swopnil8848@users.noreply.github.com>
2026-04-07 21:06:29 +0545

    feat: add dto validation on auth service - ( micro service )

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AuthServiceModule } from './auth-service.module';
+import { SERVICE_PORTS, SERVICES } from '@app/common';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthServiceModule);
-  await app.listen(process.env.PORT ?? 3001);
+
+  // Enable Validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(SERVICE_PORTS[SERVICES.AUTH_SERVICE]);
+  console.log(`Auth service is rinning on port ${SERVICE_PORTS[SERVICES.AUTH_SERVICE]}`)
 }
 bootstrap();
